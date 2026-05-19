19e515ec70af291c62ab21670898843be8657711
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-05-19 19:51:32 +0800

    feat: print token counts

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ def main():
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
 
+    usage_metadata = object.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("usage metadata not found")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
     print("Response:")
     print(object.text)
 
