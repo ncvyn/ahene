edf00feb0236ab42d45916c40a66643263934572
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-05-20 16:44:41 +0800

    refactor: rename variable to match get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,11 +2,13 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    working_dir = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir, directory))
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
     if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
-    valid_target_dir = os.path.commonpath([working_dir, target_dir]) == working_dir
+    valid_target_dir = (
+        os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+    )
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
